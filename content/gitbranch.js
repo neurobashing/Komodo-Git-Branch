cef2ec2544e92839105ebb3ec217d4554895b861
Gregg Thomason <gregg.thomason@asti-usa.com>
2015-09-11 17:15:22 -0400

    when closing a project, revert the current branch to -

--- content/gitbranch.js
+++ content/gitbranch.js
@@ -16,7 +16,12 @@ ko.extensions.gitbranch = {};
         StatusbarElement.innerHTML = "-";
     };
     this.ViewOpened = function() {
-        log.debug("We have opened a view.");
+        //log.debug("We have opened a view.");
+    };
+    this.CurrentProjectChanged = function (){
+        if (!ko.projects.manager.currentProject) {
+            StatusbarElement.innerHTML = "-";
+        }
     };
     this.CurrentViewChanged = function() {
         var shell = require('ko/shell');
@@ -24,14 +29,14 @@ ko.extensions.gitbranch = {};
         var koFile = _window.require("ko/file");
         // you can't do this:
         //var file = require('ko/file');
-        log.debug("Current view changed.");
+        //log.debug("Current view changed.");
         var path_to_proj = ko.projects.manager.currentProject.url;
         if (path_to_proj) {
-            log.debug("We have a project path, finding git branch");
+            //log.debug("We have a project path, finding git branch");
             var project_path = koFile.dirname(path_to_proj.replace('file://', ''));
-            log.debug("project_path is " + project_path);
+            // log.debug("project_path is " + project_path);
             var commandname = "git --git-dir=" + project_path + "/.git --work-tree=" + project_path + " rev-parse --abbrev-ref HEAD";
-            log.debug("command to run is " + commandname);
+            // log.debug("command to run is " + commandname);
             var process = shell.exec(commandname, {});
             process.stdout.on('data', function(data){
                 log.debug("We have data!");
@@ -43,35 +48,8 @@ ko.extensions.gitbranch = {};
     };
 }).apply(ko.extensions.gitbranch);
 addEventListener("load", function() { setTimeout(ko.extensions.gitbranch.OnLoad, 6000); }, false);
-addEventListener("view_opened", function() { ko.extensions.gitbranch.ViewOpened() }, false);
-addEventListener("current_view_changed", function() { ko.extensions.gitbranch.CurrentViewChanged() }, false);
+addEventListener("view_opened", function() { ko.extensions.gitbranch.ViewOpened(); }, false);
+addEventListener("current_view_changed", function() { ko.extensions.gitbranch.CurrentViewChanged(); }, false);
+addEventListener("current_project_changed", ko.extensions.gitbranch.CurrentProjectChanged, false);
 //addEventListener("unload", ko.extensions.gitbranch.OnUnload, false);
 
-/*
-(function () {
-    var log = require("ko/logging").getLogger('gitbranch');
-    log.setLevel(ko.logging.LOG_DEBUG);
-    var StatusbarElement;
-    this.OnLoad = function() {
-        StatusbarElement = document.getElementById('gitbranch');
-        StatusbarElement.innerHTML = "-";
-    }
-    this.ViewOpened = function() {
-        log.debug("We have opened a view.");
-    }
-    // THIS IS THE MONEY SHOT. We want to set the current branchy state here.
-    this.CurrentViewChanged = function() {
-        StatusbarElement.innerHTML = "derp";
-        log.debug("The current view has changed.");
-    }
-    // this shows on overall window focus
-    this.FocusEvent = function() {
-        log.debug("A focus event has fired");
-    }
-
-}).apply(ko.extensions.gitbranch);
-addEventListener("load", function() { setTimeout(ko.extensions.gitbranch.OnLoad, 6000); }, false);
-addEventListener("view_opened", function() { ko.extensions.gitbranch.ViewOpened() }, false);
-addEventListener("current_view_changed", function() { ko.extensions.gitbranch.CurrentViewChanged() }, false);
-//addEventListener("unload", ko.extensions.gitbranch.OnUnload, false);
-*/
