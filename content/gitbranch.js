ac8b2873aec03fb4700a4e640c9c81b9573deefd
Nathan Rijksen <n.rijksen@gmail.com>
2015-09-12 11:51:08 -0400

    Fixed initialisation and warnings

--- content/gitbranch.js
+++ content/gitbranch.js
@@ -9,47 +9,37 @@ ko.extensions.gitbranch = {};
 
 (function () {
     var log = require('ko/logging').getLogger('gitbranch');
-    log.setLevel(ko.logging.LOG_DEBUG);
-    var StatusbarElement;
+    //log.setLevel(ko.logging.LOG_DEBUG);
+    var StatusbarText;
     this.OnLoad = function() {
-        StatusbarElement = document.getElementById('gitbranch');
-        StatusbarElement.innerHTML = "-";
-    };
-    this.ViewOpened = function() {
-        //log.debug("We have opened a view.");
-    };
-    this.CurrentProjectChanged = function (){
-        if (!ko.projects.manager.currentProject) {
-            StatusbarElement.innerHTML = "-";
-        }
+        StatusbarText = document.getElementById('gitbranch-text');
+        StatusbarText.setAttribute("value", "-");
+        
+        addEventListener("current_view_changed", ko.extensions.gitbranch.CurrentViewChanged, false);
+        addEventListener("current_project_changed", ko.extensions.gitbranch.CurrentViewChanged, false);
     };
     this.CurrentViewChanged = function() {
         var shell = require('ko/shell');
-        var _window = require("ko/windows").getMain();
-        var koFile = _window.require("ko/file");
+        var koFile = require("ko/file");
         // you can't do this:
         //var file = require('ko/file');
         //log.debug("Current view changed.");
-        var path_to_proj = ko.projects.manager.currentProject.url;
-        if (path_to_proj) {
+        var project = ko.projects.manager.currentProject;
+        if (project && project.url) {
             //log.debug("We have a project path, finding git branch");
-            var project_path = koFile.dirname(path_to_proj.replace('file://', ''));
+            var project_path = koFile.dirname(ko.uriparse.URIToPath(project.url));
             // log.debug("project_path is " + project_path);
             var commandname = "git --git-dir=" + project_path + "/.git --work-tree=" + project_path + " rev-parse --abbrev-ref HEAD";
             // log.debug("command to run is " + commandname);
             var process = shell.exec(commandname, {});
             process.stdout.on('data', function(data){
-                log.debug("We have data!");
-                StatusbarElement.innerHTML = data.trim();
+                //log.debug("We have data!");
+                StatusbarText.setAttribute("value", data.trim());
             });
         } else {
-            StatusbarElement.innerHTML = '-';
+            StatusbarText.setAttribute("value", "-");
         }
     };
 }).apply(ko.extensions.gitbranch);
-addEventListener("load", function() { setTimeout(ko.extensions.gitbranch.OnLoad, 6000); }, false);
-addEventListener("view_opened", function() { ko.extensions.gitbranch.ViewOpened(); }, false);
-addEventListener("current_view_changed", function() { ko.extensions.gitbranch.CurrentViewChanged(); }, false);
-addEventListener("current_project_changed", ko.extensions.gitbranch.CurrentProjectChanged, false);
-//addEventListener("unload", ko.extensions.gitbranch.OnUnload, false);
+addEventListener("komodo-post-startup", ko.extensions.gitbranch.OnLoad, false);
 
